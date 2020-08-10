a8a6f7eb6b61784f949f626527149e30acdd6674
SereinYx <lxserein@163.com>
2020-08-10 22:08:40 +0800

    新增 ArrayQueue，LoopQueue 性能对比

---
+++ src/queue/TestQueueTime.java
@@ -0,0 +1,40 @@
+package queue;
+
+import java.util.Random;
+
+/**
+ * @ClassName TestQueueTime
+ * @Description
+ * @Author serein
+ * @Date 2020-08-10 22:01
+ */
+public class TestQueueTime {
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue Time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue Time: " + time2 + " s");
+
+    }
+
+    private static double testQueue(Queue<Integer> queue, int opCount){
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+}
