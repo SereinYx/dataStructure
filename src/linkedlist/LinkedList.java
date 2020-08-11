c0780c1dcdf10f2081f1a30b0b99191e07186262
SereinYx <lxserein@163.com>
2020-08-11 21:55:15 +0800

    add LinkedList

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,161 @@
+package linkedlist;
+
+/**
+ * @ClassName LinkedList
+ * @Description
+ * @Author serein
+ * @Date 2020-08-11 20:18
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList(){
+        this.dummyHead = new Node();
+    }
+
+    // 获取链表中元素的个数
+    public int getSize(){
+        return size;
+    }
+
+    // 返回链表是否为空
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // 在链表的index(0-based)位置添加新的元素e
+    // 在链表中不是一个常用的操作，练习用
+    public void add(int index, E e){
+        if (index < 0 || index > size){
+            throw new IllegalArgumentException("Add failed. Illegal index");
+        }
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    // 在链表头添加元素e
+    public void addFirst(E e){
+        add(0, e);
+    }
+
+    // 在链表尾端添加元素e
+    public void addLast(E e){
+        add(size, e);
+    }
+
+    // 获取链表的第index(0-based)个位置的元素
+    public E get(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Get failed. Illegal index");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public void set(int index, E e){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Set failed. Illegal index");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    public boolean contains(E e){
+        Node cur = dummyHead.next;
+        while (cur != null){
+            if (cur.e.equals(e)){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public E remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Remove failed. Illegal index");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node delNode = prev.next;
+        prev.next = delNode.next;
+        delNode.next = null;
+        size--;
+        return delNode.e;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next)
+            builder.append(cur.e).append("->");
+        builder.append("NULL");
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            list.addFirst(i);
+            System.out.println(list);
+        }
+
+        list.add(2, 666);
+        System.out.println(list);
+
+        list.remove(2);
+        System.out.println(list);
+
+        list.removeFirst();
+        System.out.println(list);
+
+        list.removeLast();
+        System.out.println(list);
+    }
+}
